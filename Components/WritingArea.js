5b89d4d2a946b43a7a94fccff24484994a9038eb
Usaid <mohdu3106@gmail.com>
2024-08-19 11:13:30 +0530

    update

--- Components/WritingArea.js
+++ Components/WritingArea.js
@@ -1,8 +1,11 @@
 const WritingArea = () =>{
     return(
+        <>
         <div className="text-area-div">
             <textarea autoFocus></textarea>
         </div>
+            <progress value={70} max={100}></progress>
+            </>
     )
 }
 
