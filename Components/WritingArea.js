d2f739790f42aec66d90590e3c47b19b67a09a27
Usaid <mohdu3106@gmail.com>
2024-08-22 15:14:03 +0530

    update

--- Components/WritingArea.js
+++ Components/WritingArea.js
@@ -10,40 +10,50 @@ const WritingArea = () =>{
     const [time, setTime] = useState(0)
     const [percentage,setPercentage] = useState(0)
     
+    useEffect(()=>{
+        let timer
+      if(timerStarted){
+        timer = setInterval(()=>{
+        setTime(prev =>{
+            if(prev>=sessionTime * 60){
+                clearInterval(timer)
+                setTimerStarted(false)
+                return prev
+            }
+            return prev + 1
+        })
+        },1000)
+      }
+
+      return () => clearInterval(timer)
+    },[timerStarted,sessionTime])
 
     useEffect(()=>{
+        console.log(sessionTime)
         console.log(time + 1)
           setPercentage(time / (sessionTime * 60) * 100)
-    },[time])
-
-    const copyText = () =>{
-       navigator.clipboard.writeText(text)
-       alert("copied Text")
-    }
-
-    const startTimer = () =>{
-        if(!timerStarted){
-            setTimerStarted(true)
-            let timer
-
-            timer = setInterval(()=>{
-                setTime(prev => {
-                    if(prev >= sessionTime * 60){
-                        clearInterval(timer)
-                        setTimerStarted(false)
-                        return prev
-                    }
-                    return prev + 1
-                })
-            },1000)
-        }
-
-        // console.log(time)
-    }
+    },[time,sessionTime])
 
     const handleChange = (e) =>{
         setText(e.target.value)
-        startTimer()
+        if(!timerStarted && e.target.value){
+        setTimerStarted(true)
+        }  
+    }
+
+    const copyText = () =>{
+        navigator.clipboard.writeText(text)
+        alert("copied Text")
+    }
+    
+    const preventSelection = (e) =>{
+    e.target.selectionStart = e.target.selectionEnd
+    }
+
+    const handleKeyDown = (e) =>{
+        if(e.ctrlKey){
+            preventSelection(e)
+        }
     }
  
     return(
@@ -57,9 +67,17 @@ const WritingArea = () =>{
                 <li>Touch</li>
             </ul>
             <div className="text-area-container">
-            <textarea value={text} onChange={(e)=>handleChange(e)} autoFocus>
+            <textarea spellCheck="false"
+            placeholder="start writing here..."
+            onSelect={preventSelection}
+            onKeyUp={preventSelection}
+            onMouseUp={preventSelection}
+            onKeyDown={handleKeyDown}
+            onChange={handleChange}
+            value={text}
+            autoFocus>
             </textarea>
-            {text &&  <button className="copy-btn" onClick={()=>copyText()}>Copy</button>}
+            {text && !timerStarted &&  <button className="copy-btn" onClick={()=>copyText()}>Copy</button>}
           
             </div>
             <div className="wotd-div">
