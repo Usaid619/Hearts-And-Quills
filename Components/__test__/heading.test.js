03c60cfe00153a49b8fbb151cc8d1c31f97f4f20
Usaid <mohdu3106@gmail.com>
2024-09-07 23:46:49 +0530

    update

--- Components/__test__/heading.test.js
+++ Components/__test__/heading.test.js
@@ -2,10 +2,20 @@ import { render, screen } from "@testing-library/react";
 import WritingArea from "../WritingArea";
 import "@testing-library/jest-dom"
 
-test("check if the writingArea is loading",()=>{
-    render(<WritingArea/>)
-    const heading = screen.getByPlaceholderText("start writing here...")
-
-    // assertion
-    expect(heading).toBeInTheDocument()
+describe("checking header Elements",()=>{
+    test("check if the placeHolder elem is loading",()=>{
+        render(<WritingArea/>)
+        const placeHolder = screen.getByPlaceholderText("start writing here...")
+    
+        // assertion
+        expect(placeHolder).toBeInTheDocument()
+    })
+    
+    test("check if blur elem is loading",()=>{
+        render(<WritingArea/>)
+        const blurElem = screen.getByRole("paragraph")
+        
+        expect(blurElem).toBeInTheDocument()
+    })
 })
+
