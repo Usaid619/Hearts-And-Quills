b10eab76402bc23fcc9b4a6ddd95b904a35083e1
Usaid <mohdu3106@gmail.com>
2024-11-07 14:43:32 +0530

    updated logos

--- Components/Form.js
+++ Components/Form.js
@@ -95,7 +95,7 @@ const Form = ({isSignUpForm}) =>{
                 <input required ref={mail} id="mail" placeholder="example@gmail.com"/>
 
                 <label htmlFor="password">Enter Password</label>
-                <input required type="text" ref={password} id="password" placeholder="must be at least 6 characters"/>
+                <input required type="password" ref={password} id="password" placeholder="must be at least 6 characters"/>
 
                 <p className="errorMessage">{errorMessage}</p>
 
